901e5e5263934a0ccee78cff1f677a7ba2502e60
nasaraaa <159721509+nasaraaa@users.noreply.github.com>
2024-04-19 13:12:20 +0400

    js

---
+++ main.js
@@ -0,0 +1,51 @@
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const slidesWrapper = document.querySelector(".slides-wrapper");
+
+const startAutoSlides = document.querySelector(".start-slide");
+const stopAutoSlides = document.querySelector(".stop-slide");
+ 
+let currentSlide = 0;
+
+function loadSlide(){
+    slides.forEach((el,index)=>{
+        if(index===currentSlide){
+            el.classList.add("active");
+        }else{
+            el.classList.remove("active")
+        }
+    })
+}
+
+function goToNextSlide(){
+    if(currentSlide===slides.length-1){
+        currentSlide = 0;
+    }else{
+        currentSlide++;
+    }
+    loadSlide()
+}
+
+function goToPrevSlide() {
+	if (currentSlide === 0) {
+		currentSlide = slides.length - 1;
+	} else {
+		currentSlide -= 1;
+	}
+	loadSlide();
+}
+slides[currentSlide].classList.add("active");
+
+let sliderIntervalId = null;
+
+
+slidesWrapper.addEventListener("mouseenter",()=>{
+    sliderIntervalId = setInterval(goToNextSlide, 5000);
+    console.log("nasara")
+})
+
+slidesWrapper.addEventListener("mouseleave", () => {
+	clearInterval()
+});
+
